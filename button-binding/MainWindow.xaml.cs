50d15997af5ed71d15da807fe655ef6e4656ff25
cool-Blue <cool.blue@y7mail.com>
2016-11-13 03:11:07 +1100

    cleaned up data binding for the view and teh view model

--- button-binding/MainWindow.xaml.cs
+++ button-binding/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls.Primitives;
+using ButtonBindingViewModel;
 using ObservableControls;
 
 namespace button_binding
@@ -10,46 +10,26 @@ namespace button_binding
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public ViewModel VM { get; set; }
-		public View V { get; set; }
-		public MainWindow ()
-		{
-			InitializeComponent();
-			V = new View();
-			VM = new ViewModel(button2);
-			DataContext = this;
-		}
+		public ObservableButton Button0 { get; set; }
+		public ObservableButton Button1 { get; set; }
+		public ObservableToggleButton Button2 { get; set; }
 
 		/// <summary>
 		/// Contains pure view state and UI behaviour
 		/// has no references to the view, it's bound to by the XAML View
 		/// </summary>
-		public class View
+		public MainWindow ()
 		{
-			public ObservableButton Button0 { get; set; }
-			public ObservableButton Button1 { get; set; }
-			public ObservableToggleButton Button2 { get; set; }
+			// build the view
+			Button0 = new ObservableButton(new List<string> { "Paused", "Logging" }, false);
+			Button1 = new ObservableButton(new List<string> { "Paused", "Logging" }, false);
+			Button2 = new ObservableToggleButton(new List<string> { "Log All", "Log VBA" }, false);
 
-			public View ()
-			{
-				Button0 = new ObservableButton(new List<string> { "Paused", "Logging" }, false);
-				Button1 = new ObservableButton(new List<string> { "Paused", "Logging" }, false);
-				Button2 = new ObservableToggleButton(new List<string> { "Log All", "Log VBA" }, false);
-			}
+			InitializeComponent();
+
+			// build the view model and connect it to the view
+			DataContext = new ViewModel(button2);
 		}
 
-		/// <summary>
-		/// mock model
-		/// This is a remote control interface bound to a button on the view
-		/// </summary>
-		public class ViewModel
-		{
-			public ToggleAutomation Button2 { get; set; }
-
-			public ViewModel (ToggleButton tb)
-			{
-				Button2 = new ToggleAutomation(tb);
-			}
-		}
 	}
 }
